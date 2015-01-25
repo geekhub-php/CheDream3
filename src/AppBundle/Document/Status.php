cc92d708973c899f87162a2a803e2eb2982d42c0
Valik Grinevich <valik.v1per@gmail.com>
2015-01-25 22:28:38 +0200

    add status document

---
+++ src/AppBundle/Document/Status.php
@@ -0,0 +1,82 @@
+<?php
+namespace AppBundle\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * Status
+ *
+ * @ODM\Document(collection="status", repositoryClass="AppBundle\Repository\CommonRepository")
+ */
+class Status implements EventInterface
+{
+    const SUBMITTED            = 'submitted';
+    const REJECTED             = 'rejected';
+    const COLLECTING_RESOURCES = 'collecting-resources';
+    const IMPLEMENTING         = 'implementing';
+    const COMPLETED            = 'completed';
+    const SUCCESS              = 'success';
+    const FAIL                 = 'fail';
+
+    /**
+     * @var integer
+     *
+     * @ODM\Id
+     */
+    protected $id;
+
+    /**
+     * @var string
+     * @return string
+     *
+     * @ODM\Field(type="string")
+     */
+    protected $title;
+
+    /**
+     * @var \DateTime
+     *
+     * @Gedmo\Timestampable(on="create")
+     * @ODM\Field(type="date")
+     */
+    protected $createdAt;
+
+    /**
+     * @ODM\ReferenceOne(targetDocument="Dream", inversedBy="statuses")
+     */
+    protected $dream;
+
+    /**
+     * Set createdAt
+     *
+     * @param  \DateTime $createdAt
+     * @return Status
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    public function getEventImage()
+    {
+        return $this->getDream()->getMediaPoster();
+    }
+
+    public function getEventTitle()
+    {
+        return sprintf('Dream "%s", has changed status to "%s"', $this->getDream()->getTitle(), $this->getTitle());
+    }
+}
