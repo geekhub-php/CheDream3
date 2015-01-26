fe099bdf3b4edc01ec20a6c37590b72b744215cb
shubaivan <shuba.ivan@yandex.ru>
2015-01-26 23:33:29 +0200

    WorkContribute/Resource fixtures

---
+++ src/AppBundle/DataFixtures/MongoDB/LoadWork.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\DataFixtures\MongoDB;
+
+use Hautelook\AliceBundle\Alice\DataFixtureLoader;
+use Nelmio\Alice\Fixtures;
+
+class LoadWork extends DataFixtureLoader
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function getFixtures()
+    {
+        return  array(
+            __DIR__ . '/WorkResource.yml',
+
+        );
+    }
+}
