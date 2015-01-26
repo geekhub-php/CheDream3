5ba663504d36266df77eb652b027d02f855b86fb
Fillin <kozeyandrey@gmail.com>
2015-01-26 20:04:50 +0200

    Install AliceBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,8 @@ class AppKernel extends Kernel
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Hautelook\AliceBundle\HautelookAliceBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
