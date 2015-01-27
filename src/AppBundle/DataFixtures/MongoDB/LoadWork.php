7bb1d59c589065964a5ce00aaffabd2a11ea3e4e
Valik Grinevich <valik.v1per@gmail.com>
2015-01-27 21:41:00 +0200

    update fixtures

--- src/AppBundle/DataFixtures/MongoDB/LoadWork.php
+++ src/AppBundle/DataFixtures/MongoDB/LoadWork.php
@@ -13,7 +13,7 @@ class LoadWork extends DataFixtureLoader
     protected function getFixtures()
     {
         return  array(
-            __DIR__ . '/WorkResource.yml',
+            __DIR__ . '/fixtures.yml',
         );
     }
 }
