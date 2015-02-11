4091ff3d62386e7df282dfe82f04bb44ffa94484
Fillin <kozeyandrey@gmail.com>
2015-02-11 22:37:41 +0200

    Create StatusController(code of crude)

--- src/AppBundle/Controller/StatusController.php
+++ src/AppBundle/Controller/StatusController.php
@@ -7,16 +7,52 @@ use FOS\RestBundle\Controller\FOSRestController;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use FOS\RestBundle\Controller\Annotations\View as RestView;
 use FOS\RestBundle\View\View;
+use FOS\RestBundle\Request\ParamFetcher;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
 
 class StatusController extends FOSRestController
 {
+//    /**
+//     * Get Status,
+//     *
+//     * @ApiDoc(
+//     * resource = true,
+//     * description = "Gets all statuses",
+//     * output="array<AppBundle\Document\Status>",
+//     * statusCodes = {
+//     *      200 = "Returned when successful",
+//     *      404 = "Returned when the status is not found"
+//     * }
+//     * )
+//     *
+//     * RestView()
+//     * @param
+//     * @return View
+//     *
+//     * @throws NotFoundHttpException when page not exist
+//     */
+//    public function getStatusAction()
+//    {
+//        $manager = $this->get('doctrine_mongodb')->getManager();
+//        $status = $manager->getRepository('AppBundle:Status')->findAll();
+//        $restView = View::create();
+//
+//        if (count($status) == 0) {
+//            $restView->setStatusCode(204);
+//        }
+//
+//        $restView->setData($status);
+//
+//        return $restView;
+//    }
+
     /**
-     * Get Status,
+     * Gets Dreams by status,
      *
      * @ApiDoc(
      * resource = true,
-     * description = "Gets all statuses",
-     * output="array<AppBundle\Document\Status>",
+     * description = "Gets Dreams by status",
+     * output =   { "class" = "AppBundle\Document\Dream", "collection" = true, "collectionName" = "status" },
      * statusCodes = {
      *      200 = "Returned when successful",
      *      404 = "Returned when the status is not found"
@@ -24,22 +60,50 @@ class StatusController extends FOSRestController
      * )
      *
      * RestView()
-     * @param
+     *
+     * @QueryParam(name="status", requirements="[a-z]+", description="Status")
+     *
+     * @param  ParamFetcher $paramFetcher
      * @return View
      *
      * @throws NotFoundHttpException when page not exist
      */
-    public function getStatusAction()
+    public function getDreamsAction(ParamFetcher $paramFetcher)
     {
         $manager = $this->get('doctrine_mongodb')->getManager();
-        $status = $manager->getRepository('AppBundle:Status')->findAll();
+
+        switch ($paramFetcher->get('status')) {
+            case 'submitted':
+                $status = 'submitted';
+                break;
+            case 'rejected':
+                $status = 'rejected';
+                break;
+            case 'collecting-resources':
+                $status = 'collecting-resources';
+                break;
+            case 'implementing':
+                $status = 'implementing';
+                break;
+            case 'completed':
+                $status = 'completed';
+                break;
+            case 'success':
+                $status = 'success';
+                break;
+            case 'fail':
+                $status = 'fail';
+                break;
+        }
+
+        $dreams = $manager->getRepository('AppBundle:Dream')->findByStatuses($status);
         $restView = View::create();
 
-        if (count($status) == 0) {
+        if (count($dreams) == 0) {
             $restView->setStatusCode(204);
         }
 
-        $restView->setData($status);
+        $restView->setData($dreams);
 
         return $restView;
     }
