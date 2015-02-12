cabf7065436b48dd839675e2412b8a7d23008400
Fillin <kozeyandrey@gmail.com>
2015-02-12 20:32:46 +0200

    Add pagination in EquipmentResourceController

--- src/AppBundle/Controller/EquipmentResourceController.php
+++ src/AppBundle/Controller/EquipmentResourceController.php
@@ -7,6 +7,10 @@ use FOS\RestBundle\Controller\FOSRestController;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use FOS\RestBundle\Controller\Annotations\View as RestView;
 use FOS\RestBundle\View\View;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
+use FOS\RestBundle\Request\ParamFetcher;
 
 class EquipmentResourceController extends FOSRestController
 {
@@ -23,25 +27,37 @@ class EquipmentResourceController extends FOSRestController
      * }
      * )
      *
+     * @QueryParam(name="limit", requirements="\d+", default="10", description="Count resources contributes at one page")
+     * @QueryParam(name="page", requirements="\d+", default="1", description="Number of page to be shown")
      *
-     * RestView()
-     * @param
+     * @RestView
+     *
+     * @param  ParamFetcher $paramFetcher
+     * @param  Request $request
      * @return View
      *
-     * @throws NotFoundHttpException when page not exist
+     * @throws NotFoundHttpException when not exist
      */
-    public function getEquipmentResourcesAction()
+    public function getEquipmentResourcesAction(Request $request, ParamFetcher $paramFetcher)
     {
         $manager = $this->get('doctrine_mongodb')->getManager();
-        $equipmentResources = $manager->getRepository('AppBundle:EquipmentResource')->findAll();
-        $restView = View::create();
+        $equipmentResourcesQuery = $manager->createQueryBuilder('AppBundle:EquipmentResource')->getQuery();
 
-        if (count($equipmentResources) == 0) {
-            $restView->setStatusCode(204);
+        if (count($equipmentResourcesQuery) == 0) {
+            throw new Exception("204 No Content");
         }
 
-        $restView->setData($equipmentResources);
+        $limit = $paramFetcher->get('limit');
+        $page = $paramFetcher->get('page');
 
-        return $restView;
+
+        $paginator  = $this->get('knp_paginator');
+        $equipmentResourcesQuery = $paginator->paginate(
+            $equipmentResourcesQuery,
+            $request->query->get('page', $page),
+            $limit
+        );
+
+        return $equipmentResourcesQuery;
     }
 }
