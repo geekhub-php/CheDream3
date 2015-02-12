09e5b0c9af8635b75cd025f5a42eb35d7df5d7c6
Fillin <kozeyandrey@gmail.com>
2015-02-12 21:03:18 +0200

    Add pagination in FinancialResourcesController

--- src/AppBundle/Controller/FinancialResourceController.php
+++ src/AppBundle/Controller/FinancialResourceController.php
@@ -7,6 +7,10 @@ use FOS\RestBundle\Controller\FOSRestController;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use FOS\RestBundle\Controller\Annotations\View as RestView;
 use FOS\RestBundle\View\View;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
+use FOS\RestBundle\Request\ParamFetcher;
 
 class FinancialResourceController extends FOSRestController
 {
@@ -23,25 +27,37 @@ class FinancialResourceController extends FOSRestController
      * }
      * )
      *
+     * @QueryParam(name="limit", requirements="\d+", default="10", description="Count financial resources at one page")
+     * @QueryParam(name="page", requirements="\d+", default="1", description="Number of page to be shown")
      *
-     * RestView()
-     * @param
+     * @RestView
+     *
+     * @param  ParamFetcher $paramFetcher
+     * @param  Request $request
      * @return View
      *
-     * @throws NotFoundHttpException when page not exist
+     * @throws NotFoundHttpException when not exist
      */
-    public function getFinancialResourcesAction()
+    public function getFinancialResourcesAction(Request $request, ParamFetcher $paramFetcher)
     {
         $manager = $this->get('doctrine_mongodb')->getManager();
-        $financialResources = $manager->getRepository('AppBundle:FinancialResource')->findAll();
-        $restView = View::create();
+        $financialResourcesQuery = $manager->createQueryBuilder('AppBundle:EquipmentResource')->getQuery();
 
-        if (count($financialResources) == 0) {
-            $restView->setStatusCode(204);
+        if (count($financialResourcesQuery) == 0) {
+            throw new Exception("204 No Content");
         }
 
-        $restView->setData($financialResources);
+        $limit = $paramFetcher->get('limit');
+        $page = $paramFetcher->get('page');
 
-        return $restView;
+
+        $paginator  = $this->get('knp_paginator');
+        $financialResourcesQuery = $paginator->paginate(
+            $financialResourcesQuery,
+            $request->query->get('page', $page),
+            $limit
+        );
+
+        return $financialResourcesQuery;
     }
 }
