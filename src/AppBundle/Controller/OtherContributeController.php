eeff307fdaaeb2c1dfc502a8919ec420039f9314
Fillin <kozeyandrey@gmail.com>
2015-02-12 21:04:39 +0200

    Add pagination in OtherContributeController

--- src/AppBundle/Controller/OtherContributeController.php
+++ src/AppBundle/Controller/OtherContributeController.php
@@ -7,6 +7,10 @@ use FOS\RestBundle\Controller\FOSRestController;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use FOS\RestBundle\Controller\Annotations\View as RestView;
 use FOS\RestBundle\View\View;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
+use FOS\RestBundle\Request\ParamFetcher;
 
 class OtherContributeController extends FOSRestController
 {
@@ -23,24 +27,37 @@ class OtherContributeController extends FOSRestController
      * }
      * )
      *
-     * RestView()
-     * @param
+     * @QueryParam(name="limit", requirements="\d+", default="10", description="Count other contributes at one page")
+     * @QueryParam(name="page", requirements="\d+", default="1", description="Number of page to be shown")
+     *
+     * @RestView
+     *
+     * @param  ParamFetcher $paramFetcher
+     * @param  Request $request
      * @return View
      *
-     * @throws NotFoundHttpException when page not exist
+     * @throws NotFoundHttpException when not exist
      */
-    public function getOtherContributesAction()
+    public function getOtherContributesAction(Request $request, ParamFetcher $paramFetcher)
     {
         $manager = $this->get('doctrine_mongodb')->getManager();
-        $contribute = $manager->getRepository('AppBundle:OtherContribute')->findAll();
-        $restView = View::create();
+        $contributesQuery = $manager->createQueryBuilder('AppBundle:EquipmentResource')->getQuery();
 
-        if (count($contribute) == 0) {
-            $restView->setStatusCode(204);
+        if (count($contributesQuery) == 0) {
+            throw new Exception("204 No Content");
         }
 
-        $restView->setData($contribute);
+        $limit = $paramFetcher->get('limit');
+        $page = $paramFetcher->get('page');
 
-        return $restView;
+
+        $paginator  = $this->get('knp_paginator');
+        $contributesQuery = $paginator->paginate(
+            $contributesQuery,
+            $request->query->get('page', $page),
+            $limit
+        );
+
+        return $contributesQuery;
     }
 }
