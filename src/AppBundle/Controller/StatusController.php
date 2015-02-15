030eeaea35716f469412098aa602081c16012a41
Fillin <kozeyandrey@gmail.com>
2015-02-15 23:56:37 +0200

    uncomment function getStatus

--- src/AppBundle/Controller/StatusController.php
+++ src/AppBundle/Controller/StatusController.php
@@ -12,39 +12,39 @@ use FOS\RestBundle\Controller\Annotations\QueryParam;
 
 class StatusController extends FOSRestController
 {
-//    /**
-//     * Get Status,
-//     *
-//     * @ApiDoc(
-//     * resource = true,
-//     * description = "Gets all statuses",
-//     * output="array<AppBundle\Document\Status>",
-//     * statusCodes = {
-//     *      200 = "Returned when successful",
-//     *      404 = "Returned when the status is not found"
-//     * }
-//     * )
-//     *
-//     * RestView()
-//     * @param
-//     * @return View
-//     *
-//     * @throws NotFoundHttpException when page not exist
-//     */
-//    public function getStatusAction()
-//    {
-//        $manager = $this->get('doctrine_mongodb')->getManager();
-//        $status = $manager->getRepository('AppBundle:Status')->findAll();
-//        $restView = View::create();
-//
-//        if (count($status) == 0) {
-//            $restView->setStatusCode(204);
-//        }
-//
-//        $restView->setData($status);
-//
-//        return $restView;
-//    }
+    /**
+     * Get Status,
+     *
+     * @ApiDoc(
+     * resource = true,
+     * description = "Gets all statuses",
+     * output="array<AppBundle\Document\Status>",
+     * statusCodes = {
+     *      200 = "Returned when successful",
+     *      404 = "Returned when the status is not found"
+     * }
+     * )
+     *
+     * RestView()
+     * @param
+     * @return View
+     *
+     * @throws NotFoundHttpException when page not exist
+     */
+    public function getStatusAction()
+    {
+        $manager = $this->get('doctrine_mongodb')->getManager();
+        $status = $manager->getRepository('AppBundle:Status')->findAll();
+        $restView = View::create();
+
+        if (count($status) == 0) {
+            $restView->setStatusCode(204);
+        }
+
+        $restView->setData($status);
+
+        return $restView;
+    }
 
     /**
      * Gets Dreams by status,
