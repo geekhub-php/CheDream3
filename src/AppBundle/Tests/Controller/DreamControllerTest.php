630e7e2ed8a41e132179088bdb3848c9fa87f9db
Fillin <kozeyandrey@gmail.com>
2015-02-18 13:41:16 +0200

    Create simple test on filters

--- src/AppBundle/Tests/Controller/DreamControllerTest.php
+++ src/AppBundle/Tests/Controller/DreamControllerTest.php
@@ -4,14 +4,30 @@ namespace AppBundle\Tests\Controller;
 
 class DreamControllerTest extends AbstractApiTest
 {
-    public function testGetDreamsAction()
+    /**
+     * @dataProvider providerData
+     */
+    public function testGetDreamsAction($status,$limit,$sort_by,$sort_order)
     {
         $client   = static::createClient();
-        $crawler  = $client->request('GET', '/dreams');
+
+        $crawler  = $client->request('GET', '/dreams?limit=4&status=submitted&sort_by=status_update&sort_order=DESC');
 
         $response = $client->getResponse();
 
         $this->assertJsonResponse($response, 200);
+
+        $this->assertTrue(
+            $client->getResponse()->headers->contains(
+                'Content-Type',
+                'application/json'
+            )
+        );
+
+        $this->assertEquals($client->getRequest()->query->get('status'),$status);
+        $this->assertEquals($client->getRequest()->query->get('limit'),$limit);
+        $this->assertEquals($client->getRequest()->query->get('sort_by'),$sort_by);
+        $this->assertEquals($client->getRequest()->query->get('sort_order'),$sort_order);
     }
 
     public function testGetDreamAction()
@@ -22,5 +38,11 @@ class DreamControllerTest extends AbstractApiTest
         $response = $client->getResponse();
 
         $this->assertJsonResponse($response, 200);
+    }
+
+    public function providerData(){
+        return [
+            ['submitted',4,'status_update','DESC']
+        ];
     }
 }
