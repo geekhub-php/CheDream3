3755b01910feab975225f239cd873077895034f7
Valik Grinevich <valik.v1per@gmail.com>
2015-02-22 19:07:11 +0200

    update object update service

--- src/AppBundle/Services/ObjectUpdater.php
+++ src/AppBundle/Services/ObjectUpdater.php
@@ -19,7 +19,17 @@ class ObjectUpdater
         $properties = $reflect->getProperties(\ReflectionProperty::IS_PUBLIC | \ReflectionProperty::IS_PROTECTED);
 
         foreach ($properties as $property) {
+            $prop = $accessor->getValue($property, 'name');
 
+            $uc_prop = ucfirst($prop);
+            $get_prop = 'get'.$uc_prop;
+            $set_prop = 'set'.$uc_prop;
+
+            if ($objectNew->$get_prop() !== null) {
+                $objectOld->$set_prop($objectNew->$get_prop());
+            }
         }
+
+        return $objectOld;
     }
 }
