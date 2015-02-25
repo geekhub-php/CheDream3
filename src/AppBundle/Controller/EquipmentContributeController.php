94e5d5c38823bb4f4e085a1bd6ea93697bad04b3
Valik Grinevich <valik.v1per@gmail.com>
2015-02-25 22:30:57 +0200

    add post method for equipment contribute

--- src/AppBundle/Controller/EquipmentContributeController.php
+++ src/AppBundle/Controller/EquipmentContributeController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use FOS\RestBundle\Controller\FOSRestController;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
@@ -56,5 +57,62 @@ class EquipmentContributeController extends FOSRestController
         );
 
         return $equipmentQuery;
+    }
+
+    /**
+     * @ApiDoc(
+     *      resource = true,
+     *      description = "create single equipment contribute",
+     *      parameters = {
+     *          {"name" = "equipment_resource", "dataType" = "array<AppBundle\Document\EquipmentResource>", "required" = true, "description" = "resource that contributet" },
+     *          {"name" = "quantity", "dataType" = "integer", "required" = true, "description" = "count contributet resources" }
+     *      },
+     *      statusCodes = {
+     *          204 = "Returned when successful create",
+     *          404 = "Returned when dream is not found"
+     *      }
+     * )
+     *
+     * @param Request $request
+     * @param $slug
+     *
+     * @return View
+     */
+    public function postEquipmentContributesAction(Request $request, $slug)
+    {
+        $data = $request->request->all();
+        $user = $this->getUser();
+
+        $dm = $this->get('doctrine.odm.mongodb.document_manager');
+
+        $slugResource = $request->request->get('equipment_contribute');
+
+        $dream = $this->get('doctrine_mongodb.odm.document_manager')
+                        ->getRepository('AppBundle:Dream')
+                        ->findOneBySlug($slug);
+
+        $equipment_resource = $this->get('doctrine_mongodb.odm.document_manager')
+                                    ->getRepository('AppBundle:EquipmentResource')
+                                    ->findOneBySlug($slugResource);
+
+        $view = View::create();
+
+        if (!$dream) {
+            $view->setStatusCode(404);
+        } else {
+            $data = $this->get('serializer')->serialize($data, 'json');
+            $equipment_contribute = $this->get('serializer')->deserialize($data, 'AppBundle\Document\EquipmentContribute', 'json');
+
+            $equipment_contribute->setEquipmentResource($equipment_resource);
+            $equipment_contribute->setDream($dream);
+            $equipment_contribute->setUser($user);
+
+            $view->setStatusCode(204);
+
+            $dm->persist($equipment_contribute);
+            $dm->flush();
+        }
+
+        return $view;
     }
 }
