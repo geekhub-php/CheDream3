f12f4c059c4adf25711f235355d90958609bb146
Valik <valik.v1per@gmail.com>
2015-03-04 09:32:14 +0300

    Update ObjectUpdater, add dream financial resource

--- src/AppBundle/Tests/Services/ObjectUpdaterTest.php
+++ src/AppBundle/Tests/Services/ObjectUpdaterTest.php
@@ -5,7 +5,7 @@ namespace AppBundle\Tests\Services;
 use AppBundle\Tests\Controller\AbstractApiTest;
 use AppBundle\Document\Dream;
 
-class ObjectUpdaterTest extends AbstractApiTest
+class ObjectUpdaterTest extends \PHPUnit_Framework_TestCase
 {
     public function testUpdateObject()
     {
@@ -15,10 +15,15 @@ class ObjectUpdaterTest extends AbstractApiTest
                            ->get('doctrine_mongodb.odm.document_manager')
                            ->getRepository('AppBundle:Dream')
                            ->findOneBySlug('sunt');
+        
+        $financial_resource = new FinancialResource()
+        $financial_resource->setTitle('ex');
+        $financial_resource->setQuantity(50);
 
         $dreamTmp = new Dream();
         $dreamTmp->setTitle('test1');
         $dreamTmp->setDescription('test2');
+        $dreamTmp->addDreamFinancialResource($financial_resource);
 
         $dreamNew = $client->getContainer()->get('app.services.object_updater')->updateObject($dreamOld, $dreamTmp);
 
