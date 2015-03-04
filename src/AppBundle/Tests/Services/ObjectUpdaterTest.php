2783c59a682264dc2cac2f0ee2b2f605df54ef9e
Valik <valik.v1per@gmail.com>
2015-03-04 10:05:21 +0300

    add new financial resource in ObjectUpdaterTest

--- src/AppBundle/Tests/Services/ObjectUpdaterTest.php
+++ src/AppBundle/Tests/Services/ObjectUpdaterTest.php
@@ -20,15 +20,21 @@ class ObjectUpdaterTest extends AbstractApiTest
         $financial_resource = new FinancialResource();
         $financial_resource->setTitle('ex');
         $financial_resource->setQuantity(50);
+        
+        $financial_resource2 = new FinancialResource();
+        $financial_resource2->setTitle('myTesttt11');
+        $financial_resource2->setQuantity(5021);
 
         $dreamTmp = new Dream();
         $dreamTmp->setTitle('test1');
         $dreamTmp->setDescription('test2');
         $dreamTmp->addDreamFinancialResource($financial_resource);
+        $dreamTmp->addDreamFinancialResource($financial_resource2);
 
         $dreamNew = $client->getContainer()->get('app.services.object_updater')->updateObject($dreamOld, $dreamTmp);
 
         $this->assertEquals($dreamNew->getTitle(), $dreamTmp->getTitle());
         $this->assertEquals($dreamNew->getDescription(), $dreamTmp->getDescription());
+        $this->assertEquals($dreamNew->getDreamFinancialResource()->count(), $dreamTmp->getDreamFinancialResource()->count());
     }
 }
