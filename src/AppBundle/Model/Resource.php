ff9c5bb3600b9253e30341566859356d5563e694
Valik Grinevich <valik.v1per@gmail.com>
2015-03-07 21:40:09 +0200

    update resource model file

--- src/AppBundle/Model/Resource.php
+++ src/AppBundle/Model/Resource.php
@@ -6,8 +6,8 @@ use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
 
 /**
  * @ODM\Document
- * @ODM\InheritanceType("SINGLE_TABLE")
- * @ODM\DiscriminatorColumn(name="discr", type="string")
+ * @ODM\InheritanceType("SINGLE_COLLECTION")
+ * @ODM\DiscriminatorColumn(name="type", type="string")
  * @ODM\DiscriminatorMap({"resource" = "Resource", "work_resource" = "WorkResource", "financial_resource" = "FinancialResource", "equipment_resource" = "EquipmentResource"})
  */
 class Resource
@@ -16,4 +16,41 @@ class Resource
      * @ODM\Id(strategy="AUTO")
      */
     protected $id;
+
+    /**
+     * @ODM\ReferenceOne(targetDocument="AppBundle\Document\Dream")
+     */
+    protected $dream;
+
+    /**
+     * @ODM\ReferenceMany(targetDocument="AppBundle\Document\Contributes")
+     */
+    protected $contributes = [];
+
+    /**
+     * @var string $title
+     *
+     * @ODM\Field(type="string")
+     * @Expose()
+     * @Type("string")
+     */
+    protected $title;
+
+    /**
+     * @var date $createdAt
+     *
+     * @ODM\Field(type="date")
+     * @Expose()
+     * @Type("DateTime")
+     */
+    protected $createdAt;
+
+    /**
+     * @var float $quantity
+     *
+     * @ODM\Field(type="float")
+     * @Expose()
+     * @Type("float")
+     */
+    protected $quantity;
 }
