2da1cb0446b8c072382ebd0fe9bae672968be7ee
Valik Grinevich <valik.v1per@gmail.com>
2015-03-07 21:39:39 +0200

    update contribute model file

--- src/AppBundle/Model/Contribute.php
+++ src/AppBundle/Model/Contribute.php
@@ -3,32 +3,43 @@
 namespace AppBundle\Model;
 
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+use JMS\Serializer\Annotation\ExclusionPolicy;
+use JMS\Serializer\Annotation\Expose;
+use JMS\Serializer\Annotation\Type;
 
 /**
  * @ODM\Document
- * @ODM\InheritanceType("SINGLE_TABLE")
- * @ODM\DiscriminatorColumn(name="discr", type="string")
+ * @ODM\InheritanceType("SINGLE_COLLECTION")
+ * @ODM\DiscriminatorField(name="type", type="string")
  * @ODM\DiscriminatorMap({"contribute" = "Contribute", "work_contribute" = "WorkContribute", "financial_contribute" = "FinancialContribute", "equipment_contribute" = "EquipmentContribute", "other_contribute" = "OtherContribute"})
+ * @ExclusionPolicy("all")
  */
 class Contribute
 {
     /**
      * @ODM\Id(strategy="AUTO")
+     * @Expose()
      */
     protected $id;
 
     /**
      * @ODM\ReferenceOne(targetDocument="AppBundle\Document\Dream")
+     * @Expose()
      */
     protected $dream;
 
     /**
      * @ODM\ReferenceMany(targetDocument="AppBundle\Document\Resource")
+     * @Expose()
      */
     protected $resources;
 
     /**
-     * @ODM\Field(type="integer")
+     * @var float $quantity
+     *
+     * @ODM\Field(type="float")
+     * @Expose()
+     * @Type("float")
      */
     protected $quantity;
 
@@ -43,6 +54,16 @@ class Contribute
 
     /**
      * @ODM\ReferenceOne(targetDocument="AppBundle\Document\User")
+     * @Expose()
      */
     protected $user;
+
+    /**
+     * @var date $createdAt
+     *
+     * @ODM\Field(type="date")
+     * @Expose()
+     * @Type("DateTime")
+     */
+    protected $createdAt;
 }
