8c604e2c8bed14c0b357df35eca3723f896780d3
fillin <kozeyandrey@gmail.com>
2015-03-09 10:17:48 +0200

    Create getValidation method

--- src/AppBundle/Controller/AbstractController.php
+++ src/AppBundle/Controller/AbstractController.php
@@ -3,11 +3,24 @@
 namespace AppBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Response;
 
 abstract class AbstractController extends FOSRestController
 {
     public function getMongoDbManager()
     {
         return $this->get('doctrine.odm.mongodb.document_manager');
+    }
+
+    public function getValidation($object)
+    {
+        $validator = $this->get('validator');
+        $errors = $validator->validate($object);
+
+        if (count($errors) > 0) {
+            $errorsString = (string) $errors;
+
+            return new Response($errorsString);
+        }
     }
 }
